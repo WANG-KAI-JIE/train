9d0ba58b2925d34bdbca70559f15ecb0269f612b
wkj <1729298243@qq.com>
2023-11-21 18:52:08 +0800

    乘车人编辑页面开发

--- member/src/main/java/top/kjwang/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/top/kjwang/train/member/req/PassengerSaveReq.java
@@ -1,5 +1,6 @@
 package top.kjwang.train.member.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -22,7 +23,6 @@ import java.util.Date;
 public class PassengerSaveReq {
 	private Long id;
 
-	// @NotNull(message = "【会员ID】不能为空")
 	private Long memberId;
 
 	@NotBlank(message = "【名字】不能为空")
@@ -34,7 +34,9 @@ public class PassengerSaveReq {
 	@NotBlank(message = "【旅客类型】不能为空")
 	private String type;
 
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date createTime;
 
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date updateTime;
 }
