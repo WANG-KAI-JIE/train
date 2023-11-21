8916fd988f077bcaae03e3200ab9932a315a54e7
wkj <1729298243@qq.com>
2023-11-21 19:54:21 +0800

    制作实体类生成器

--- generator/src/main/java/top/kjwang/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/top/kjwang/train/generator/gen/ServerGenerator.java
@@ -6,12 +6,12 @@ import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 import top.kjwang.train.generator.util.DbUtil;
+import top.kjwang.train.generator.util.Field;
 import top.kjwang.train.generator.util.FreemarkerUtil;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author moqi
@@ -56,28 +56,49 @@ public class ServerGenerator {
 		DbUtil.password = password.getText();
 
 
-		// 示例：表名 mqxu_test
-		// Domain = MqxuTest
+		// 示例：表名 kjwang_test
+		// Domain = kjwangTest
 		String Domain = domainObjectName.getText();
-		// domain = mqxuTest
+		// domain = kjwangTest
 		String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
-		// do_main = mqxu_test
+		// do_main = kjwang_test
 		String do_main = tableName.getText().replaceAll("_", "-");
+		// 表中文名
+		String tableNameCn = DbUtil.getTableComment(tableName.getText());
+		List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
+		Set<String> typeSet = getJavaTypes(fieldList);
 
 		// 组装参数
 		Map<String, Object> param = new HashMap<>();
 		param.put("Domain", Domain);
 		param.put("domain", domain);
 		param.put("do_main", do_main);
+		param.put("tableNameCn", tableNameCn);
+		param.put("fieldList", fieldList);
+		param.put("typeSet", typeSet);
 		System.out.println("组装参数：" + param);
 
-		gen(Domain, param, "service");
-		gen(Domain, param, "controller");
+		gen(Domain, param, "service", "service");
+		gen(Domain, param, "controller", "controller");
+		gen(Domain, param, "req", "saveReq");
+		gen(Domain, param, "req", "saveReq");
+
 	}
 
-	private static void gen(String Domain, Map<String, Object> param, String target) throws IOException, TemplateException {
+	/**
+	 * 获取所有的Java类型，使用Set去重
+	 */
+	private static Set<String> getJavaTypes(List<Field> fieldList) {
+		Set<String> set = new HashSet<>();
+		for (Field field : fieldList) {
+			set.add(field.getJavaType());
+		}
+		return set;
+	}
+
+	private static void gen(String Domain, Map<String, Object> param, String packageName, String target) throws IOException, TemplateException {
 		FreemarkerUtil.initConfig(target + ".ftl");
-		String toPath = serverPath + target + "/";
+		String toPath = serverPath + packageName + "/";
 		new File(toPath).mkdirs();
 		String Target = target.substring(0, 1).toUpperCase() + target.substring(1);
 		String fileName = toPath + Domain + Target + ".java";
---
+++ member/src/main/java/top/kjwang/train/member/req/PassengerSaveReq1.java
@@ -0,0 +1,42 @@
+package top.kjwang.train.member.req;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author kjwang
+ * @date 2023/11/21 14:30
+ * @description PassengerSaveReq
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PassengerSaveReq1 {
+	private Long id;
+
+	private Long memberId;
+
+	@NotBlank(message = "【名字】不能为空")
+	private String name;
+
+	@NotBlank(message = "【身份证】不能为空")
+	private String idCard;
+
+	@NotBlank(message = "【旅客类型】不能为空")
+	private String type;
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	private Date createTime;
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	private Date updateTime;
+}
