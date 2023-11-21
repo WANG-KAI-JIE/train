b9900877343ed139b687c7638aede6f39aef55e0
wkj <1729298243@qq.com>
2023-11-21 19:36:20 +0800

    集成 DOM4j 读取 xml-读取 pom.xml ，得到当前持久层的 xml 文件

--- generator/src/main/java/top/kjwang/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/top/kjwang/train/generator/gen/ServerGenerator.java
@@ -1,12 +1,13 @@
 package top.kjwang.train.generator.gen;
 
-import freemarker.template.TemplateException;
-import top.kjwang.train.generator.util.FreemarkerUtil;
+import org.dom4j.Document;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
 /**
  * @author kjwang
  * @date 2023/11/21 19:31
@@ -15,15 +16,24 @@ import java.util.Map;
 
 public class ServerGenerator {
 	static String toPath = "generator/src/main/java/top/kjwang/train/generator/test/";
+	static String pomPath = "generator/pom.xml";
 
 	static {
 		new File(toPath).mkdirs();
 	}
 
-	public static void main(String[] args) throws IOException, TemplateException {
-		FreemarkerUtil.initConfig("test.ftl");
-		Map<String, Object> param = new HashMap<>();
-		param.put("domain", "Test1");
-		FreemarkerUtil.generator(toPath + "Test1.java", param);
+	public static void main(String[] args) throws Exception {
+		SAXReader saxReader = new SAXReader();
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("pom", "http://maven.apache.org/POM/4.0.0");
+		saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+		Document document = saxReader.read(pomPath);
+		Node node = document.selectSingleNode("//pom:configurationFile");
+		System.out.println(node.getText());
+
+		//FreemarkerUtil.initConfig("test.ftl");
+		//Map<String, Object> param = new HashMap<>();
+		//param.put("domain", "Test1");
+		//FreemarkerUtil.generator(toPath + "Test1.java", param);
 	}
 }
