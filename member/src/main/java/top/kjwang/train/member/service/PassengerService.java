f48294e296681cabbf0deeb8ee73585d3e514f9a
wkj <1729298243@qq.com>
2023-11-21 16:33:35 +0800

    集成PageHelper实现后端分页-集成 PageHelper，实现 sql 分页功能

--- member/src/main/java/top/kjwang/train/member/service/PassengerService.java
+++ member/src/main/java/top/kjwang/train/member/service/PassengerService.java
@@ -3,6 +3,7 @@ package top.kjwang.train.member.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import top.kjwang.train.common.context.LoginMemberContext;
@@ -44,6 +45,7 @@ public class PassengerService {
 		if (ObjectUtil.isNotNull(req.getMemberId())) {
 			criteria.andMemberIdEqualTo(req.getMemberId());
 		}
+		PageHelper.startPage(2,2);
 		List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
 		return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
 	}
