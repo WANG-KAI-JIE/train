5e1994d2773bf230e9ddec22639f0a88d556984d
wkj <1729298243@qq.com>
2023-11-22 08:29:54 +0800

    增加 admin 管理员模块-复制 web 模块，重命名为 admin

---
+++ admin/src/components/the-header.vue
@@ -0,0 +1,45 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <div style="float: right; color: white">
+      您好：{{ member.mobile }}
+      <router-link to="/login" style="color: white">退出登录</router-link>
+    </div>
+    <a-menu
+      v-model:selectedKeys="selectedKeys"
+      theme="dark"
+      mode="horizontal"
+      :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome"> <coffee-outlined /> 欢迎 </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <user-outlined /> 乘车人管理
+        </router-link>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+import store from "@/store";
+import router from "@/router";
+
+let member = store.state.member;
+const selectedKeys = ref([]);
+
+watch(
+  () => router.currentRoute.value.path,
+  (newValue) => {
+    console.log("watch", newValue);
+    selectedKeys.value = [];
+    selectedKeys.value.push(newValue);
+  },
+  { immediate: true }
+);
+</script>
+
+<style scoped></style>
