7aedc5b743afff5ea31bcfbced7d883480a68eb7
wkj <1729298243@qq.com>
2023-11-22 08:47:11 +0800

    增加 business 业务模块

---
+++ business/src/main/java/top/kjwang/business/controller/TestController.java
@@ -0,0 +1,18 @@
+package top.kjwang.business.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kjwang
+ * @date 2023/11/22 08:40
+ * @description TestController
+ */
+
+@RestController
+public class TestController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World!\nBusiness!";
+	}
+}
