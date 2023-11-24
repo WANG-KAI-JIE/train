56f21530d418c2d6739ef3da2ef92099b1b6c5f8
wkj <1729298243@qq.com>
2023-11-24 09:36:27 +0800

    完善每日车站管理页面功能-增加日期和车次查询条件

--- business/src/main/java/top/kjwang/train/business/req/DailyTrainStationQueryReq.java
+++ business/src/main/java/top/kjwang/train/business/req/DailyTrainStationQueryReq.java
@@ -1,11 +1,36 @@
 package top.kjwang.train.business.req;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import top.kjwang.train.common.req.PageReq;
 
+import java.util.Date;
+
 public class DailyTrainStationQueryReq extends PageReq {
-@Override
-public String toString() {
-return "DailyTrainStationQueryReq{" +
-"} " + super.toString();
-}
+
+	public String getTrainCode() {
+		return trainCode;
+	}
+
+	public void setTrainCode(String trainCode) {
+		this.trainCode = trainCode;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	private String trainCode;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date date;
+
+	@Override
+	public String toString() {
+		return "DailyTrainStationQueryReq{" +
+				"} " + super.toString();
+	}
 }
