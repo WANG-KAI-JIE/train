38a30406e4ca8ab23501e5cb8a97722c038a243b
wkj <1729298243@qq.com>
2023-11-24 14:01:14 +0800

    完善每日车厢管理功能-增加日期和车次查询条件

--- business/src/main/java/top/kjwang/train/business/req/DailyTrainCarriageQueryReq.java
+++ business/src/main/java/top/kjwang/train/business/req/DailyTrainCarriageQueryReq.java
@@ -1,11 +1,36 @@
 package top.kjwang.train.business.req;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import top.kjwang.train.common.req.PageReq;
 
+import java.util.Date;
+
 public class DailyTrainCarriageQueryReq extends PageReq {
-@Override
-public String toString() {
-return "DailyTrainCarriageQueryReq{" +
-"} " + super.toString();
-}
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date date;
+
+	private String trainCode;
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getTrainCode() {
+		return trainCode;
+	}
+
+	public void setTrainCode(String trainCode) {
+		this.trainCode = trainCode;
+	}
+
+	@Override
+	public String toString() {
+		return "DailyTrainCarriageQueryReq{" +
+				"} " + super.toString();
+	}
 }
