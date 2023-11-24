0b2ff1addc2ccb3c91d6a9223a7134b2d82e8750
wkj <1729298243@qq.com>
2023-11-24 14:13:56 +0800

    修复每日车厢根据车次查找

--- admin/src/views/main/business/daily-train-carriage.vue
+++ admin/src/views/main/business/daily-train-carriage.vue
@@ -1,24 +1,35 @@
 <template>
   <p>
     <a-space>
-      <a-date-picker v-model:value="params.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" />
-      <train-select-view v-model="params.trainCode" width="200px"></train-select-view>
+      <a-date-picker
+        v-model:value="params.date"
+        valueFormat="YYYY-MM-DD"
+        placeholder="请选择日期"
+      />
+      <train-select-view
+        v-model:value="params.trainCode"
+        width="200px"
+      ></train-select-view>
       <a-button type="primary" @click="handleQuery()">查找</a-button>
       <a-button type="primary" @click="onAdd">新增</a-button>
     </a-space>
   </p>
-  <a-table :dataSource="dailyTrainCarriages"
-           :columns="columns"
-           :pagination="pagination"
-           @change="handleTableChange"
-           :loading="loading">
+  <a-table
+    :dataSource="dailyTrainCarriages"
+    :columns="columns"
+    :pagination="pagination"
+    @change="handleTableChange"
+    :loading="loading"
+  >
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
         <a-space>
           <a-popconfirm
-              title="删除后不可恢复，确认删除?"
-              @confirm="onDelete(record)"
-              ok-text="确认" cancel-text="取消">
+            title="删除后不可恢复，确认删除?"
+            @confirm="onDelete(record)"
+            ok-text="确认"
+            cancel-text="取消"
+          >
             <a style="color: red">删除</a>
           </a-popconfirm>
           <a @click="onEdit(record)">编辑</a>
@@ -33,23 +44,38 @@
       </template>
     </template>
   </a-table>
-  <a-modal v-model:visible="visible" title="每日车厢" @ok="handleOk"
-           ok-text="确认" cancel-text="取消">
-    <a-form :model="dailyTrainCarriage" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+  <a-modal
+    v-model:visible="visible"
+    title="每日车厢"
+    @ok="handleOk"
+    ok-text="确认"
+    cancel-text="取消"
+  >
+    <a-form
+      :model="dailyTrainCarriage"
+      :label-col="{ span: 4 }"
+      :wrapper-col="{ span: 20 }"
+    >
       <a-form-item label="日期">
-        <a-date-picker v-model:value="dailyTrainCarriage.date" valueFormat="YYYY-MM-DD"
-                       placeholder="请选择日期"/>
+        <a-date-picker
+          v-model:value="dailyTrainCarriage.date"
+          valueFormat="YYYY-MM-DD"
+          placeholder="请选择日期"
+        />
       </a-form-item>
       <a-form-item label="车次编号">
-        <train-select-view v-model:value="dailyTrainCarriage.trainCode"/>
+        <train-select-view v-model:value="dailyTrainCarriage.trainCode" />
       </a-form-item>
       <a-form-item label="厢序">
-        <a-input v-model:value="dailyTrainCarriage.index"/>
+        <a-input v-model:value="dailyTrainCarriage.index" />
       </a-form-item>
       <a-form-item label="座位类型">
         <a-select v-model:value="dailyTrainCarriage.seatType">
-          <a-select-option v-for="item in SEAT_TYPE_ARRAY" :key="item.code"
-                           :value="item.code">
+          <a-select-option
+            v-for="item in SEAT_TYPE_ARRAY"
+            :key="item.code"
+            :value="item.code"
+          >
             {{ item.desc }}
           </a-select-option>
         </a-select>
@@ -58,7 +84,7 @@
       <!--        <a-input v-model:value="dailyTrainCarriage.seatCount"/>-->
       <!--      </a-form-item>-->
       <a-form-item label="排数">
-        <a-input v-model:value="dailyTrainCarriage.rowCount"/>
+        <a-input v-model:value="dailyTrainCarriage.rowCount" />
       </a-form-item>
       <!--      <a-form-item label="列数">-->
       <!--        <a-input v-model:value="dailyTrainCarriage.colCount"/>-->
@@ -68,8 +94,8 @@
 </template>
 
 <script setup>
-import {ref, onMounted} from 'vue';
-import {notification} from "ant-design-vue";
+import { ref, onMounted } from "vue";
+import { notification } from "ant-design-vue";
 import axios from "axios";
 import TrainSelectView from "@/components/train-select";
 
@@ -97,44 +123,44 @@ const pagination = ref({
 let loading = ref(false);
 const columns = [
   {
-    title: '日期',
-    dataIndex: 'date',
-    key: 'date',
+    title: "日期",
+    dataIndex: "date",
+    key: "date",
   },
   {
-    title: '车次编号',
-    dataIndex: 'trainCode',
-    key: 'trainCode',
+    title: "车次编号",
+    dataIndex: "trainCode",
+    key: "trainCode",
   },
   {
-    title: '厢序',
-    dataIndex: 'index',
-    key: 'index',
+    title: "厢序",
+    dataIndex: "index",
+    key: "index",
   },
   {
-    title: '座位类型',
-    dataIndex: 'seatType',
-    key: 'seatType',
+    title: "座位类型",
+    dataIndex: "seatType",
+    key: "seatType",
   },
   {
-    title: '座位数',
-    dataIndex: 'seatCount',
-    key: 'seatCount',
+    title: "座位数",
+    dataIndex: "seatCount",
+    key: "seatCount",
   },
   {
-    title: '排数',
-    dataIndex: 'rowCount',
-    key: 'rowCount',
+    title: "排数",
+    dataIndex: "rowCount",
+    key: "rowCount",
   },
   {
-    title: '列数',
-    dataIndex: 'colCount',
-    key: 'colCount',
+    title: "列数",
+    dataIndex: "colCount",
+    key: "colCount",
   },
   {
-    title: '操作',
-    dataIndex: 'operation'
-  }
+    title: "操作",
+    dataIndex: "operation",
+  },
 ];
 
 const onAdd = () => {
@@ -148,81 +174,87 @@ const onEdit = (record) => {
 };
 
 const onDelete = (record) => {
-  axios.delete("/business/admin/daily-train-carriage/delete/" + record.id).then((response) => {
-    const data = response.data;
-    if (data.success) {
-      notification.success({description: "删除成功！"});
-      handleQuery({
-        page: pagination.value.current,
-        size: pagination.value.pageSize,
-      });
-    } else {
-      notification.error({description: data.message});
-    }
-  });
+  axios
+    .delete("/business/admin/daily-train-carriage/delete/" + record.id)
+    .then((response) => {
+      const data = response.data;
+      if (data.success) {
+        notification.success({ description: "删除成功！" });
+        handleQuery({
+          page: pagination.value.current,
+          size: pagination.value.pageSize,
+        });
+      } else {
+        notification.error({ description: data.message });
+      }
+    });
 };
 
 const handleOk = () => {
-  axios.post("/business/admin/daily-train-carriage/save", dailyTrainCarriage.value).then((response) => {
-    let data = response.data;
-    if (data.success) {
-      notification.success({description: "保存成功！"});
-      visible.value = false;
-      handleQuery({
-        page: pagination.value.current,
-        size: pagination.value.pageSize
-      });
-    } else {
-      notification.error({description: data.message});
-    }
-  });
+  axios
+    .post("/business/admin/daily-train-carriage/save", dailyTrainCarriage.value)
+    .then((response) => {
+      let data = response.data;
+      if (data.success) {
+        notification.success({ description: "保存成功！" });
+        visible.value = false;
+        handleQuery({
+          page: pagination.value.current,
+          size: pagination.value.pageSize,
+        });
+      } else {
+        notification.error({ description: data.message });
+      }
+    });
 };
 
 let params = ref({
   trainCode: null,
-  date: null
+  date: null,
 });
 
 const handleQuery = (param) => {
   if (!param) {
     param = {
       page: 1,
-      size: pagination.value.pageSize
+      size: pagination.value.pageSize,
     };
   }
   loading.value = true;
-  axios.get("/business/admin/daily-train-carriage/query-list", {
-    params: {
-      page: param.page,
-      size: param.size,
-      trainCode: params.value.trainCode,
-      date: params.value.date
-    }
-  }).then((response) => {
-    loading.value = false;
-    let data = response.data;
-    if (data.success) {
-      dailyTrainCarriages.value = data.content.list;
-      // 设置分页控件的值
-      pagination.value.current = param.page;
-      pagination.value.total = data.content.total;
-    } else {
-      notification.error({description: data.message});
-    }
-  });
+  axios
+    .get("/business/admin/daily-train-carriage/query-list", {
+      params: {
+        page: param.page,
+        size: param.size,
+        trainCode: params.value.trainCode,
+        date: params.value.date,
+      },
+    })
+    .then((response) => {
+      loading.value = false;
+      let data = response.data;
+      if (data.success) {
+        dailyTrainCarriages.value = data.content.list;
+        // 设置分页控件的值
+        pagination.value.current = param.page;
+        pagination.value.total = data.content.total;
+      } else {
+        notification.error({ description: data.message });
+      }
+    });
 };
 
 const handleTableChange = (pagination) => {
   handleQuery({
     page: pagination.current,
-    size: pagination.pageSize
+    size: pagination.pageSize,
   });
 };
 
 onMounted(() => {
   handleQuery({
     page: 1,
-    size: pagination.value.pageSize
+    size: pagination.value.pageSize,
   });
 });
 </script>
