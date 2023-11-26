4be8ac7617b1de7081b343472e7d219b6b602bc3
wkj <1729298243@qq.com>
2023-11-26 18:28:36 +0800

    为余票页面增加查询条件

--- business/src/main/java/top/kjwang/train/business/req/DailyTrainTicketQueryReq.java
+++ business/src/main/java/top/kjwang/train/business/req/DailyTrainTicketQueryReq.java
@@ -1,11 +1,40 @@
 package top.kjwang.train.business.req;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 import top.kjwang.train.common.req.PageReq;
 
+import java.util.Date;
+
+
+@Getter
+@Setter
 public class DailyTrainTicketQueryReq extends PageReq {
-@Override
-public String toString() {
-return "DailyTrainTicketQueryReq{" +
-"} " + super.toString();
-}
+	/**
+	 * 日期
+	 */
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date date;
+
+	/**
+	 * 日期编号
+	 */
+	private String trainCode;
+
+	/**
+	 * 出发站
+	 */
+	private String start;
+
+	/**
+	 * 到达站
+	 */
+	private String end;
+
+	@Override
+	public String toString() {
+		return "DailyTrainTicketQueryReq{" +
+				"} " + super.toString();
+	}
 }
