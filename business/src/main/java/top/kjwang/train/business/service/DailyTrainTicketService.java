97f6f4fb89e7e3c98863000a5b6d1181af743917
wkj <1729298243@qq.com>
2023-11-27 20:36:26 +0800

    演示因数据库本身就没数据而造成缓存穿透

--- business/src/main/java/top/kjwang/train/business/service/DailyTrainTicketService.java
+++ business/src/main/java/top/kjwang/train/business/service/DailyTrainTicketService.java
@@ -59,6 +59,11 @@ public class DailyTrainTicketService {
 		}
 	}
 
+	@Cacheable(value = "DailyTrainTicketService.queryList3")
+	public PageResp<DailyTrainTicketQueryResp> queryList3(DailyTrainTicketQueryReq req) {
+		LOG.info("测试缓存击穿");
+		return null;
+	}
 
 	@CachePut(value = "DailyTrainTicketService.queryList")
 	public PageResp<DailyTrainTicketQueryResp> queryList2(DailyTrainTicketQueryReq req) {
