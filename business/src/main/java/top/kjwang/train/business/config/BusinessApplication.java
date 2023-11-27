a8783fde2097d05eb6c61613305c7c7ce881627c
wkj <1729298243@qq.com>
2023-11-27 20:19:39 +0800

    SpringBoot 内置缓存

--- business/src/main/java/top/kjwang/train/business/config/BusinessApplication.java
+++ business/src/main/java/top/kjwang/train/business/config/BusinessApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
@@ -19,6 +20,7 @@ import org.springframework.core.env.Environment;
 @ComponentScan("top.kjwang")
 @MapperScan("top.kjwang.train.*.mapper")
 @EnableFeignClients("top.kjwang.train.business.feign")
+@EnableCaching
 public class BusinessApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
