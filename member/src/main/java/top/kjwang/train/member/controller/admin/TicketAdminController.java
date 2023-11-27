7d486d4a3ef069ba652801b69a9a946517f127a8
wkj <1729298243@qq.com>
2023-11-27 16:14:05 +0800

    增加一个 feign 接口，保存车票信息

--- member/src/main/java/top/kjwang/train/member/controller/admin/TicketAdminController.java
+++ member/src/main/java/top/kjwang/train/member/controller/admin/TicketAdminController.java
@@ -15,25 +15,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/ticket")
 public class TicketAdminController {
 
-@Resource
-private TicketService ticketService;
+	@Resource
+	private TicketService ticketService;
 
-@PostMapping("/save")
-public CommonResp<Object> save(@Valid @RequestBody TicketSaveReq req) {
-    ticketService.save(req);
-    return new CommonResp<>();
-    }
+	@GetMapping("/query-list")
+	public CommonResp<PageResp<TicketQueryResp>> queryList(@Valid TicketQueryReq req) {
+		PageResp<TicketQueryResp> list = ticketService.queryList(req);
+		return new CommonResp<>(list);
+	}
 
-    @GetMapping("/query-list")
-    public CommonResp<PageResp<TicketQueryResp>> queryList(@Valid TicketQueryReq req) {
-        PageResp<TicketQueryResp> list = ticketService.queryList(req);
-            return new CommonResp<>(list);
-            }
-
-            @DeleteMapping("/delete/{id}")
-            public CommonResp<Object> delete(@PathVariable Long id) {
-                ticketService.delete(id);
-                return new CommonResp<>();
-                }
-
-                }
+}
--- member/src/main/java/top/kjwang/train/member/service/TicketService.java
+++ member/src/main/java/top/kjwang/train/member/service/TicketService.java
@@ -5,6 +5,7 @@ import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import top.kjwang.train.common.req.MemberTicketReq;
 import top.kjwang.train.common.resp.PageResp;
 import top.kjwang.train.common.util.SnowUtil;
 import top.kjwang.train.member.domain.Ticket;
@@ -23,49 +24,44 @@ import java.util.List;
 @Service
 public class TicketService {
 
-private static final Logger LOG = LoggerFactory.getLogger(TicketService.class);
+	private static final Logger LOG = LoggerFactory.getLogger(TicketService.class);
 
-@Resource
-private TicketMapper ticketMapper;
+	@Resource
+	private TicketMapper ticketMapper;
 
-public void save(TicketSaveReq req) {
-DateTime now = DateTime.now();
-Ticket ticket = BeanUtil.copyProperties(req, Ticket.class);
-if (ObjectUtil.isNull(ticket.getId())) {
-ticket.setId(SnowUtil.getSnowflakeNextId());
-ticket.setCreateTime(now);
-ticket.setUpdateTime(now);
-ticketMapper.insert(ticket);
-} else {
-ticket.setUpdateTime(now);
-ticketMapper.updateByPrimaryKey(ticket);
+	public void save(MemberTicketReq req) throws Exception {
+		DateTime now = DateTime.now();
+		Ticket ticket = BeanUtil.copyProperties(req, Ticket.class);
+		ticket.setId(SnowUtil.getSnowflakeNextId());
+		ticket.setCreateTime(now);
+		ticket.setUpdateTime(now);
+		ticketMapper.insert(ticket);
+	}
+
+	public PageResp<TicketQueryResp> queryList(TicketQueryReq req) {
+		TicketExample ticketExample = new TicketExample();
+		ticketExample.setOrderByClause("id desc");
+		TicketExample.Criteria criteria = ticketExample.createCriteria();
+
+		LOG.info("查询页码：{}", req.getPage());
+		LOG.info("每页条数：{}", req.getSize());
+		PageHelper.startPage(req.getPage(), req.getSize());
+		List<Ticket> ticketList = ticketMapper.selectByExample(ticketExample);
+
+		PageInfo<Ticket> pageInfo = new PageInfo<>(ticketList);
+		LOG.info("总行数：{}", pageInfo.getTotal());
+		LOG.info("总页数：{}", pageInfo.getPages());
+
+		List<TicketQueryResp> list = BeanUtil.copyToList(ticketList, TicketQueryResp.class);
+
+		PageResp<TicketQueryResp> pageResp = new PageResp<>();
+		pageResp.setTotal(pageInfo.getTotal());
+		pageResp.setList(list);
+		return pageResp;
+	}
+
+	public void delete(Long id) {
+		ticketMapper.deleteByPrimaryKey(id);
+	}
+
 }
-}
-
-public PageResp<TicketQueryResp> queryList(TicketQueryReq req) {
-    TicketExample ticketExample = new TicketExample();
-    ticketExample.setOrderByClause("id desc");
-    TicketExample.Criteria criteria = ticketExample.createCriteria();
-
-    LOG.info("查询页码：{}", req.getPage());
-    LOG.info("每页条数：{}", req.getSize());
-    PageHelper.startPage(req.getPage(), req.getSize());
-    List<Ticket> ticketList = ticketMapper.selectByExample(ticketExample);
-
-    PageInfo<Ticket> pageInfo = new PageInfo<>(ticketList);
-    LOG.info("总行数：{}", pageInfo.getTotal());
-    LOG.info("总页数：{}", pageInfo.getPages());
-
-    List<TicketQueryResp> list = BeanUtil.copyToList(ticketList, TicketQueryResp.class);
-
-        PageResp<TicketQueryResp> pageResp = new PageResp<>();
-            pageResp.setTotal(pageInfo.getTotal());
-            pageResp.setList(list);
-            return pageResp;
-            }
-
-            public void delete(Long id) {
-            ticketMapper.deleteByPrimaryKey(id);
-            }
-
-            }
