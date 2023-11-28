633cb7336e95062fcd153fb6247283dc9ac67fc9
wkj <1729298243@qq.com>
2023-11-28 18:25:51 +0800

    Sentinel 限流不同的流控效果

--- business/src/main/java/top/kjwang/train/business/controller/TestController.java
+++ business/src/main/java/top/kjwang/train/business/controller/TestController.java
@@ -1,5 +1,6 @@
 package top.kjwang.train.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+	@SentinelResource("hello")
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello World!\nBusiness!";
