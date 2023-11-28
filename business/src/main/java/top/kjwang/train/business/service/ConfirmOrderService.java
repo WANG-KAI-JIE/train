439f34734c3682032c4e3a01e72c666465c19b6c
wkj <1729298243@qq.com>
2023-11-28 17:49:10 +0800

    使用 Sentinel 控台监控流量

--- business/src/main/java/top/kjwang/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/top/kjwang/train/business/service/ConfirmOrderService.java
@@ -8,6 +8,8 @@ import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.core.collection.CollUtil;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -106,6 +108,12 @@ public class ConfirmOrderService {
 		confirmOrderMapper.deleteByPrimaryKey(id);
 	}
 
+	public void doConfirmBlock(ConfirmOrderDoReq req, BlockException e) {
+		LOG.info("购票请求被限流：{}", req);
+		throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION);
+	}
+
+	@SentinelResource(value = "doConfirm", blockHandler = "doConfirmBlock")
 	public void doConfirm(ConfirmOrderDoReq req) {
 		String lockKey = DateUtil.formatDate(req.getDate()) + "-" + req.getTrainCode();
 		RLock lock = null;
